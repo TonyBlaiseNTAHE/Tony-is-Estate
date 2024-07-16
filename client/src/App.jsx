e5b816d5e6e893b3fd50398c584b67d1e4bffed7
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-07-16 10:58:53 +0200

    creatng the header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import SignUp from './pages/SignUp';
-import SignOut from './pages/SignOut';
-import Profile from './pages/Profile';
-import About from './pages/About';
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import Header from "./components/Header";
 
 export default function App() {
-  return (<BrowserRouter>
-  <Routes>
-    <Route path='/' element={<Home />} />
-    <Route path='/sign-up' element={<SignUp />} />
-    <Route path='/sign-out' element={<SignOut />} />
-    <Route path='/profile'element={<Profile/>} />
-    <Route path='/about'element={<About/>} />
-  </Routes>
-  </BrowserRouter>
-  )
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-out" element={<SignOut />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+export default function Header() {
+  return (
+    <header className="bg-orange-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl  mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl felx flex-wrap">
+            <span className="text-slate-500">Tony</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
+        </Link>
+        <form className="bg-orange-100 p-3 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64 "
+          />
+          <FaSearch className="text-orange-400" />
+        </form>
+        <ul className="flex gap-6">
+          <Link to="/">
+            <li className="hover:underline">Home</li>
+          </Link>
+          <Link to="/about">
+            <li className="hover:underline">About</li>
+          </Link>
+          <Link to="/sign-in">
+            <li className="hover:underline">Sign In</li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
